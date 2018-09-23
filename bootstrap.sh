98ad080acf3f589d9e16c6f45f8f7fb33c906603
Mikael Hultgren <blomma@gmail.com>
2018-09-23 20:55:10 +0200

    One before the other

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-if [ -f "$PLATFORM-bootstrap.sh" ]
+if [ -f "bootstrap-$PLATFORM.sh" ]
 then
-    source "$PLATFORM-bootstrap.sh"
+    source "bootstrap-$PLATFORM.sh"
 fi
