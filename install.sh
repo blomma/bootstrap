56a6275401b20dfe51467e6a23693a287de4dcb7
Mikael Hultgren <blomma@gmail.com>
2018-09-23 20:19:29 +0200

    Next attempt

--- install.sh
+++ install.sh
@@ -2,11 +2,4 @@ cd ~
 git clone --recursive https://github.com/blomma/bootstrap.git
 cd bootstrap
 
-PLATFORM=(uname -s | tr '[:upper:]' '[:lower:]')
-
-if [ -f "$PLATFORM-bootstrap.sh" ]
-    bash source "$PLATFORM-bootstrap.sh"
-then
-fi
-
-# bash bootstrap.sh
+bash bootstrap.sh
