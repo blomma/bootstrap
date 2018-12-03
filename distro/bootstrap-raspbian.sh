89f97d1a605259a0f0a13a43cd5a950fce215ed1
Mikael Hultgren <blomma@gmail.com>
2018-12-03 14:36:28 +0100

    More stuff

--- distro/bootstrap-raspbian.sh
+++ distro/bootstrap-raspbian.sh
@@ -1,38 +1,37 @@
 U=$(whoami)
 
 # Install linuxbrew
-sudo apt -y install build-essential curl file git
-git clone https://github.com/Linuxbrew/brew ~/.linuxbrew/Homebrew
-mkdir ~/.linuxbrew/bin
-ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
-eval $(~/.linuxbrew/bin/brew shellenv)
+sudo apt -y install build-essential curl file git vim tmux
+
+# linux brew is to slow atm for a pi since it compiles everything from scratch
+
+# git clone https://github.com/Linuxbrew/brew ~/.linuxbrew/Homebrew
+# mkdir ~/.linuxbrew/bin
+# ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+# eval $(~/.linuxbrew/bin/brew shellenv)
 
 # Update Homebrew
-brew update
+# brew update
 
 # Install brews
-brew install \
-    git \
-    go \
-    midnight-commander \
-    tig \
-    tmux \
-    vim \
-    urlview \
-    wget \
+# brew install \
+#     git \
+#     tmux \
+#     vim
 
 # Symlink everything we need
-go get -u github.com/blomma/viaduct
+wget https://github.com/blomma/viaduct/releases/download/v0.8/linux-arm-7-viaduct.tar.bz2
+tar -jxvf linux-arm-7-viaduct.tar.bz2
 
 git clone --recursive https://github.com/blomma/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
-~/go/bin/viaduct fish
-~/go/bin/viaduct mc
-~/go/bin/viaduct omf
-~/go/bin/viaduct ssh
-~/go/bin/viaduct tmux
-~/go/bin/viaduct vim
+~/bin/linux-arm-7-viaduct fish
+~/bin/linux-arm-7-viaduct mc
+~/bin/linux-arm-7-viaduct omf
+~/bin/linux-arm-7-viaduct ssh
+~/bin/linux-arm-7-viaduct tmux
+~/bin/linux-arm-7-viaduct vim
 
 sudo apt -y install fish
 sudo chsh -s /usr/bin/fish $U
