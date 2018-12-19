33c2e78c54bd9d7870fdabb6645f28af1637112b
Mikael Hultgren <blomma@gmail.com>
2018-12-20 00:22:40 +0100

    Moar

--- distro/bootstrap-ubuntu.sh
+++ distro/bootstrap-ubuntu.sh
@@ -1,11 +1,14 @@
 U=$(whoami)
 
 sudo apt update
-sudo apt install -y fish git sudo curl
+sudo apt install -y fish git sudo curl build-essential
 sudo chsh -s /usr/bin/fish $U
 
 # Install linuxbrew
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
 # Update Homebrew
 brew update
