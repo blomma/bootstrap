0a1b065e3bfda858c38ccc4195f4306a0a93e7b1
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-07-30 09:30:10 +0200

    Update bootstrap-raspbian.sh

--- distro/bootstrap-raspbian.sh
+++ distro/bootstrap-raspbian.sh
@@ -9,11 +9,10 @@ go get -u github.com/blomma/viaduct
 git clone --recursive https://github.com/blomma/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
-~/bin/linux-arm-7-viaduct fish
-~/bin/linux-arm-7-viaduct mc
-~/bin/linux-arm-7-viaduct omf
-~/bin/linux-arm-7-viaduct ssh
-~/bin/linux-arm-7-viaduct tmux
-~/bin/linux-arm-7-viaduct vim
+~/go/bin/viaduct fish
+~/go/bin/viaduct ssh
+~/go/bin/viaduct tmux
+~/go/bin/viaduct vim
+~/go/bin/viaduct git
 
 sudo chsh -s /usr/bin/fish $U
