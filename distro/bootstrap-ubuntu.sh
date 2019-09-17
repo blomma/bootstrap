40f1c9a6125e46fc5fc4897a3c888d4e1b58e09c
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-09-17 21:56:25 +0200

    Update bootstrap-ubuntu.sh

--- distro/bootstrap-ubuntu.sh
+++ distro/bootstrap-ubuntu.sh
@@ -1,27 +1,7 @@
 U=$(whoami)
 
-sudo apt update
-sudo apt install -y fish git sudo curl build-essential
-sudo chsh -s /usr/bin/fish $U
-
 # Install linuxbrew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-
-# Update Homebrew
-brew update
-
-# Install brews
-brew install \
-    git \
-    go \
-    ncdu \
-    tig \
-    tmux \
-    vim \
-    wget \
+sudo apt -y install build-essential curl file git vim tmux golang fish
 
 # Symlink everything we need
 go get -u github.com/blomma/viaduct
@@ -30,11 +10,9 @@ git clone --recursive https://github.com/blomma/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
 ~/go/bin/viaduct fish
-~/go/bin/viaduct mc
-~/go/bin/viaduct nvim
 ~/go/bin/viaduct ssh
 ~/go/bin/viaduct tmux
 ~/go/bin/viaduct vim
+~/go/bin/viaduct git
 
-# Fonts are special, we copy those
-#cp fonts/* ~/Library/Fonts/
+sudo chsh -s /usr/bin/fish $U
